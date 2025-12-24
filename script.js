bc6a8aa0e234d93e6b5619e25ffb882018e77e87
adithyaabhiram-28 <adithya.abhiram.2805@gmail.com>
2025-12-24 13:17:36 +0530

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+function login() {
+    document.getElementById("msg").textContent =
+        "Login successful (demo only)";
+}
+
+function register() {
+    document.getElementById("msg").textContent =
+        "Registration successful (demo only)";
+}
+
+function pay() {
+    document.getElementById("msg").textContent =
+        "Payment completed successfully!";
+}
